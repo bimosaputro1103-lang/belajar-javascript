7dc356fa80a2ad7a72623b490123aa0b3a8062e9
bimosaputro1103-lang <bimosaputro1103@gmail.com>
2026-07-22 23:35:21 -0700

    Belajar Javascript [Dasar] - 05 - Tipe Data

---
+++ Belajar Javascript [Dasar] - 05 - Tipe Data/script.js
@@ -0,0 +1,21 @@
+console.log("wikididiw");
+
+//ini adalah tutoril tentang tipe data
+
+let datastring= "ucup";
+console.log(datastring);
+console.log(typeof datastring)
+
+let dataNumber = 16.324;
+console.log(dataNumber);
+console.log(typeof dataNumber);
+
+let dataBoolean = false;
+console.log(dataBoolean);
+console.log(typeof dataBoolean);
+
+let dataKosong;
+console.log(typeof dataKosong);
+dataKosong = "otong";
+console.log(dataKosong);
+console.log(typeof dataKosong);
