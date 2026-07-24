4e6c721560856d79b0cb65398d78ffcf100a5ecf
bimosaputro1103-lang <bimosaputro1103@gmail.com>
2026-07-23 20:43:24 -0700

    Belajar Javascript [Dasar] - 11 - NaN dan Undefined

---
+++ Belajar Javascript [Dasar] - 11 - NaN dan Undefined/script.js
@@ -0,0 +1,19 @@
+console.log("wadidid");
+
+//NaN DAN UNDERFIND 
+
+//NaN = Not a Number
+console.log(`akar dari -1 \t:${Math.sqrt(-1)}`);
+console.log(`"ucup" / "otong" :${ "ucup" / "otong"}`);
+console.log(`"ucup" * "otong" :${ "ucup" * "otong"}`);
+console.log(`"ucup" + "otong" :${ "ucup" + "otong"}`);
+console.log(`"ucup" - "otong" :${ "ucup" - "otong"}`);
+
+let data = parseInt("test123");
+console.log(data + 5); //NaN ini tidak bisa dioperasikan lagi
+
+//underfind - dia belum tau nilainya
+let a;
+console.log(`a = ${a}`);
+console.log(Math.sqrt(4));
+console.log(console.log("test"));
