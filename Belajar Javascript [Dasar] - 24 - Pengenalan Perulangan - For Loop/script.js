aafadc68fab76ccaf2f2ce248391b2fc14d91cfe
bimosaputro1103-lang <bimosaputro1103@gmail.com>
2026-07-24 19:09:10 -0700

    Belajar Javascript [Dasar] - 24 - Pengenalan Perulangan - For Loop

---
+++ Belajar Javascript [Dasar] - 24 - Pengenalan Perulangan - For Loop/script.js
@@ -0,0 +1,22 @@
+// perulangan
+data_array = [1,2,3,4,5,12,23,4,23,42,3,42,34];
+
+//for loop
+//for(inisialisasi; kondisi; increment){}
+
+let i = 1;
+
+for( let i = 1; i<=10 ; i++ ){
+  console.log(`nilai i = ${i}`);
+i++;
+}
+
+for(let i = 1; i<= 12; i++){
+    console.log(`array ke-${i} = ${data_array[i]}`);
+}
+
+console.log("menggunakan length")
+for(let i = 0; i < data_array.length;i++){
+    console.log(`array ke-${i} = ${data_array[i]}`);
+}
+
