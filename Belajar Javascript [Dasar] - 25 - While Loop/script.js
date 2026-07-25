968621705ca1af3a15ccb0cd582e0502cc742499
bimosaputro1103-lang <bimosaputro1103@gmail.com>
2026-07-24 19:48:46 -0700

    Belajar Javascript [Dasar] - 26 - Do While Loop

--- Belajar Javascript [Dasar] - 25 - While Loop/script.js
+++ Belajar Javascript [Dasar] - 25 - While Loop/script.js
@@ -3,8 +3,39 @@
 //while(flag){
   //  body program
 //}
+console.log("awal program");
 
-while(false){
+let flag_1 = true;
+while(flag){
     console.log("ahey");
+    flag = false;
 }
+
+let flag_2 = true;
+let angka = 0;
+
+while(flag_2){
+    console.log("ahey");
+
+    if(angka==10){
+        flag_2 = false;
+    }
+
+    angka++;
+}
+
+let main_lagi = true;
+let ronde = 1;
+while(main_lagi){
+    console.log(`kamu sedang bermain game di ronde ${ronde}`);
+
+    main_lagi = confirm("apakah mau main lagi?");
+
+    ronde++;
+}
+
+
+
+console.log("akhir program")
+
 
---
+++ Belajar Javascript [Dasar] - 26 - Do While Loop/script.js
@@ -0,0 +1,16 @@
+// do while
+console.log("awal program");
+
+let uang = 0;
+
+while(uang > 0){
+  console.log("kita bisa jajan cilok pakai while loop");
+}
+
+let saldo = 0;
+
+do {
+    console.log("kita bisa jajan cilok pakai do while loop");
+} while (saldo > 0);
+
+console.log("akhir program");
