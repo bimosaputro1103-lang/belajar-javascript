8ff19ce2addf16a004d23a3a5bd3c4fab26cdd9a
bimosaputro1103-lang <bimosaputro1103@gmail.com>
2026-07-24 21:05:37 -0700

    Belajar Javascript [Dasar] - 29 - Pengenalan Fungsi

---
+++ Belajar Javascript [Dasar] - 29 - Pengenalan Fungsi/script.js
@@ -0,0 +1,22 @@
+// fungsi
+
+//function nama fungsi (argument){
+  //  body program 
+
+   // return value;
+//}
+
+//deklarasi fungi
+function sayHello(){
+    let data = "apa kabar";
+    console.log(data);
+}
+ function sayKeren(){
+    let data = "hai keren";
+    console.log(data);
+}
+
+//main prtogran
+sayHello();
+sayKeren();
+sayHello();
